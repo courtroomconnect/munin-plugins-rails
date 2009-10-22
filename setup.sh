c75243fcbe2dfddb67d60c398868cdc0712ba0ae
Matthew Schoolmaster <schoolmaster@gmail.com>
2009-10-21 18:45:15 -0600

    improved setup.sh (have i ever shell scripted before?)

--- setup.sh
+++ setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-ln -s memcached_ /etc/munin/plugins/memcached_bytes
-ln -s memcached_ /etc/munin/plugins/memcached_counters
-ln -s memcached_ /etc/munin/plugins/memcached_rates
-ln -s rails_database_time  /etc/munin/plugins/rails_database_time
-ln -s rails_passenger_memory  /etc/munin/plugins/rails_passenger_memory
-ln -s rails_passenger_status  /etc/munin/plugins/rails_passenger_status
-ln -s rails_request_duration  /etc/munin/plugins/rails_request_duration
-ln -s rails_request_error  /etc/munin/plugins/rails_request_error
-ln -s rails_requests  /etc/munin/plugins/rails_requests
-ln -s rails_view_render_time /etc/munin/plugins/rails_view_render_time
+ln -s $PWD/memcached_ /etc/munin/plugins/memcached_bytes
+ln -s $PWD/memcached_ /etc/munin/plugins/memcached_counters
+ln -s $PWD/memcached_ /etc/munin/plugins/memcached_rates
+ln -s $PWD/rails_database_time  /etc/munin/plugins/rails_database_time
+ln -s $PWD/rails_passenger_memory  /etc/munin/plugins/rails_passenger_memory
+ln -s $PWD/rails_passenger_status  /etc/munin/plugins/rails_passenger_status
+ln -s $PWD/rails_request_duration  /etc/munin/plugins/rails_request_duration
+ln -s $PWD/rails_request_error  /etc/munin/plugins/rails_request_error
+ln -s $PWD/rails_requests  /etc/munin/plugins/rails_requests
+ln -s $PWD/rails_view_render_time /etc/munin/plugins/rails_view_render_time
 
 echo "" >> /etc/munin/plugin-conf.d/munin-node
 echo "[rails*]" >> /etc/munin/plugin-conf.d/munin-node
